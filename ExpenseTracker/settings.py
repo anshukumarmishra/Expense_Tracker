6a22e09afe6f6d7bbf194066b383d08a52a26489
anshu777 <anshumishra777777@gmail.com>
2026-06-30 10:09:25 +0530

    Prepare project for Render deployment

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -20,10 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-njzny*#iu=s$=npg8fid_h_5iz9d(8kz)$mv*t%np)m08tslra'
+import os
+
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-njzny*#iu=s$=npg8fid_h_5iz9d(8kz)$mv*t%np)m08tslra",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".onrender.com",
